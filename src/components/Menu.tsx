689eb92d63b4dac76c817c0414ba29e894f776d8
吴海洋 <why@vip.local>
2023-03-02 13:40:37 +0800

    03

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,5 +6,23 @@ interface Props {
 
 export default ({ items }: Props) => {
   console.log(items);
-  return <h2>菜谱内容</h2>;
+  return (
+    <div className="section-center">
+      {items.map((menuItem) => {
+        const { id, title, img, desc, price } = menuItem;
+        return (
+          <article key={id} className="menu-item">
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">¥{price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
